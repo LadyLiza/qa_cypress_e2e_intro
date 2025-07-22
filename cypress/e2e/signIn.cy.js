85d0dfec74bcfb31cd1257d80b51c20f98f98d99
LadyLiza <elizabet132010@gmail.com>
2025-07-22 20:53:20 +0200

    add task solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,32 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  const user = {
+    username: 'liza',
+    email: 'liza@gmail.com',
+    password: 'liza1234'
+  };
 
+  before(() => {
+    cy.request({
+      method: 'POST',
+      url: 'https://conduit.mate.academy/api/users',
+      body: { user },
+      failOnStatusCode: false
+    });
+  });
+
+  it('should log in to an existing account', () => {
+    cy.visit('/user/login');
+
+    cy.contains('h1', 'Sign in').should('be.visible');
+
+    cy.get('input[type="email"]').type(user.email);
+    cy.get('input[type="password"]').type(user.password);
+    cy.get('.btn').click();
+
+    cy.url({ timeout: 10000 }).should('not.include', '/login');
+
+    cy.contains('a', user.username, { timeout: 10000 }).should('be.visible');
   });
 });
